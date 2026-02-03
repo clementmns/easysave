5fd79ee9d12f064ed5e6bc55c9df7245a0252361
Clément Omnès <clement.mns.pro@gmail.com>
2026-02-03 18:35:55 +0100

    feat: remove ChangeLanguage method to simplify language management

--- ConsoleApp/Model/Settings.cs
+++ ConsoleApp/Model/Settings.cs
@@ -1,21 +1,6 @@
-﻿using System.Globalization;
-
-namespace EasySave.ConsoleApp.Model;
+﻿namespace EasySave.ConsoleApp.Model;
 
 public class Settings
 {
     public string Language { get; set; } = "en-US";
-    
-    public void ChangeLanguage(string cultureName)
-    {
-        var culture = CultureInfo.GetCultureInfo(cultureName);
-
-        Language = cultureName;
-
-        // Change culture for all threads
-        CultureInfo.CurrentCulture = culture;
-        CultureInfo.CurrentUICulture = culture;
-        CultureInfo.DefaultThreadCurrentCulture = culture;
-        CultureInfo.DefaultThreadCurrentUICulture = culture;
-    } 
 }
