3a730a5828c17af6a273b2dbdfde6448c0bd975f
Timothé <timothe.fardella@viacesi.fr>
2026-02-03 09:42:14 +0100

    chore: create file utils architecture

--- ConsoleApp/Utils/FileUtils.cs
+++ ConsoleApp/Utils/FileUtils.cs
@@ -1,6 +1,46 @@
-﻿namespace EasySave.ConsoleApp.Utils;
+﻿using System.Security.Cryptography;
+
+namespace EasySave.ConsoleApp.Utils;
 
 public class FileUtils
 {
-    test;
+    public static long CopyFile(string source, string destination)
+    {
+        // pass
+    }
+
+    public static List<FileInfo> GetAllFiles(string directoryPath)
+    {
+        // pass
+    }
+
+    public static string ConvertToUnc(string path)
+    {
+        // pass
+    }
+
+    public static DateTime GetLastModifiedDate(string filePath)
+    {
+        // pass
+    }
+
+    public static void CreateDirectory(string path)
+    {
+        // pass
+    }
+
+    public static bool FileExists(string path)
+    {
+        // pass
+    }
+
+    public static bool DirectoryExists(string path)
+    {
+        // pass
+    }
+
+    public static long GetFileSize(string path)
+    {
+        // pass
+    }
 }
