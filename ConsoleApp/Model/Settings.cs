4f35ff83c609a7b303236e9a829f67619159c8a6
Clément Omnès <clement.mns.pro@gmail.com>
2026-02-03 15:38:39 +0100

    feat: add language property and method to change application culture

--- ConsoleApp/Model/Settings.cs
+++ ConsoleApp/Model/Settings.cs
@@ -1,6 +1,21 @@
-﻿namespace EasySave.ConsoleApp.Model;
+﻿using System.Globalization;
+
+namespace EasySave.ConsoleApp.Model;
 
 public class Settings
 {
+    public string Language { get; set; } = "en-US";
     
+    public void ChangeLanguage(string cultureName)
+    {
+        var culture = CultureInfo.GetCultureInfo(cultureName);
+
+        Language = cultureName;
+
+        // Change culture for all threads
+        CultureInfo.CurrentCulture = culture;
+        CultureInfo.CurrentUICulture = culture;
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+    } 
 }
