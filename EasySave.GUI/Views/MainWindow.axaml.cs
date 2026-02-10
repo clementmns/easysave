9d6e409bf9c17a20a710f72e96007de524fc3cc4
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-02-10 15:35:42 +0100

    feat : Add button add

--- EasySave.GUI/Views/MainWindow.axaml.cs
+++ EasySave.GUI/Views/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using EasySave.Core.Model;
 using EasySave.GUI.ViewModels;
 
 
@@ -10,5 +12,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void AddJob_Click(object? sender, RoutedEventArgs e)
+    {
     }
 }
